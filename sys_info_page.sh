ebda054ee094a171a0f2a19874482da61bdec532
John Nguyen <nguyen.john@ufl.edu>
2026-09-09 10:19:52 -0400

    First version of system project

--- sys_info_page.sh
+++ sys_info_page.sh
@@ -1,1 +1,10 @@
-First_project
+#!/bin/bash
+# Program to output a system information page
+echo "<html>
+  <head>
+      <title>Page Title</title>
+  </head>
+  <body>
+      Page body.
+  </body>
+</html>"
